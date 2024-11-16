3cded64545766ed1cae1a1e6ee96f904984d9e2e
Prince2024-des <princeawlavi83@gmail.com>
2024-11-16 01:31:33 +0000

    responsiveness for tablet

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-  
-
-  function showSidebar(){
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'flex'
-  }
-
-
-  function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'none'
-  }
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+  console.log("Showing Sidebar")
+}
 
 
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
